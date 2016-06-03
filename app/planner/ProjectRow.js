9d581d10e62790b6ad52376a5d85ff17e9046045
anrid <anridig@gmail.com>
2016-06-04 00:30:12 +0700

    Styling Time Planner.

--- app/planner/ProjectRow.js
+++ app/planner/ProjectRow.js
@@ -8,6 +8,18 @@ import './ProjectRow.scss'
 import GridOverlay from './GridOverlay'
 
 export default class ProjectRow extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+    this.toggleOpen = this.toggleOpen.bind(this)
+  }
+
+  toggleOpen () {
+    this.setState({ open: !this.state.open })
+  }
+
   render () {
     const { project } = this.props
     const startDate = Moment(this.props.pivotDate).startOf('isoWeek')
@@ -19,26 +31,36 @@ export default class ProjectRow extends Component {
       membersInfo = `${project.members.length} members.`
     }
 
+    const { open } = this.state
+    let rowCls = 'pl-time-planner-project-row-wrapper '
+    let angleCls = 'fa fa-angle-down'
+    if (open) {
+      rowCls += 'pl-time-planner-project-row-wrapper--open'
+      angleCls = 'fa fa-angle-up'
+    }
+
     return (
-      <section className='pl-time-planner-project-row'>
-        <div className='pl-time-planner-project-row__left'>
-          <div className='pl-time-planner-project-row__info'>
-            <div className={
-              'pl-time-planner-project-row__label ' +
-              'pl-time-planner-project-row__' + color
-            } />
-            <div className='pl-time-planner-project-row__title'>
-              {project.title}
+      <section className={rowCls}>
+        <section className='pl-time-planner-project-row'>
+          <div className='pl-time-planner-project-row__left'>
+            <div className='pl-time-planner-project-row__info'>
+              <div className={
+                'pl-time-planner-project-row__label ' +
+                'pl-time-planner-project-row__' + color
+              } />
+              <div className='pl-time-planner-project-row__title'>
+                {project.title}
+              </div>
+              <div className='pl-time-planner-project-row__stats'>
+                {membersInfo}
+              </div>
             </div>
-            <div className='pl-time-planner-project-row__stats'>
-              {membersInfo}
-            </div>
+            <i className={angleCls} onClick={this.toggleOpen} />
           </div>
-          <i className='fa fa-angle-down' />
-        </div>
-        <div className='pl-time-planner-project-row__right'>
-          <GridOverlay size={90} />
-        </div>
+          <div className='pl-time-planner-project-row__right'>
+            <GridOverlay size={90} />
+          </div>
+        </section>
       </section>
     )
   }
